883140f35ee82ecd295992d0a3d01f3758f9a731
worriedape <144045807+worriedape@users.noreply.github.com>
2024-10-28 12:01:51 -0600

    Add steps 2-5

--- script.js
+++ script.js
@@ -7,10 +7,55 @@ function getComputerChoice() {
     function choose(rock, paper, scissors) {
         if ( random() <= 33 ) {
          alert("Rock")
-        } else if ( random() >= 34 && random() <= 66 ) {
+        } else if ( random() > 66 ) {
          alert("Paper")
         } else (alert("Scissors"));
         }
         
     choose();
+}
+
+function getHumanChoice() {
+
+    let choice = prompt('Add R for rock, P for paper, and S for scissors.');
+
+    if ( choice === "R" ) {
+        alert("Rock")
+    } else if ( choice === "P") {
+        alert("Paper")
+    } else if ( choice === "S") {
+        alert("Scissors")
+    } else (alert("Use only R, P, or S."))
+}
+
+
+function playGame() {
+    
+ let humanScore = ;
+
+ let computerScore = ;
+
+ function playRound(humanChoice, computerChoice) {
+
+ }
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+
+
+    /*
+
+    let humanChoice = getHumanChoice();
+
+    let computerChoice = getComputerChoice();
+
+    if(humanChoice() )
+}
+
+    for (let playRound() = 0, playRound() < 6, playRound()++) {
+        playRound();
+    }
+
 }
