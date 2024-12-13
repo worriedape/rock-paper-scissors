27daf793dcfa00af68ce58ce29054242d656761f
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-12 21:57:30 -0600

    Comments for further reviewing

--- script.js
+++ script.js
@@ -8,17 +8,23 @@ outer: for (let i = 0; ; i++) {
   let humanSelection = prompt("Choose between rock, paper, and scissors");
 
   // generate random computer choice
-  let computerSelection = function () {
+  function computerSelector() {
     const paperRockScissors = ["paper", "rock", "scissors"];
     // return a random index from array paperRockScissors
     let random = Math.floor(Math.random() * paperRockScissors.length);
     // return the string attached to the index returned by random
     return paperRockScissors[random];
-  };
+  }
+
+  // each time the comparison runs the function calling computerSelector
+  // a new random value is returned, making it impossible to compare a
+  // consisting value throughout the iteration
+  // so we need to assign it to another variable
+  let computerSelection = computerSelector();
 
   //let versus = ;
 
-  console.log(humanSelection, "vs", computerSelection());
+  console.log(humanSelection, "vs", computerSelection);
 
   // compare both choices
 
@@ -43,9 +49,8 @@ outer: for (let i = 0; ; i++) {
 
 // announce winner
 //let winner = function () {
-  if (humanScore === 3) {
-    console.log("You win!");
-  } else if (computerScore === 3) {
-    console.log("Computer wins!");
-  }
-
+if (humanScore === 3) {
+  console.log("You win!");
+} else if (computerScore === 3) {
+  console.log("Computer wins!");
+}
