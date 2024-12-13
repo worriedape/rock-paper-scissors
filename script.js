b02b2ac835a03a48fa64f394dd895fdad9f54b25
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-12 19:42:08 -0600

    Add previous comparison formula

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // takes human choice
+let humanSelection;
+
 const paper = document.querySelector(".paper");
-paper.addEventListener("click", humanSelection = "paper")
+paper.addEventListener("click", (humanSelection = "paper"));
 
 const rock = document.querySelector(".rock");
 rock.addEventListener("click", (humanSelection = "rock"));
@@ -9,31 +11,35 @@ const scissors = document.querySelector(".scissors");
 scissors.addEventListener("click", (humanSelection = "scissors"));
 
 // generate random computer choice
-const paperRockScissors = ["paper", "rock", "scissors"];
-let randomRPS = function () {
+let computerSelection = function () {
+  const paperRockScissors = ["paper", "rock", "scissors"];
   // return a random index from array paperRockScissors
   let random = Math.floor(Math.random() * paperRockScissors.length);
   // return the string attached to the index returned by random
   return paperRockScissors[random];
 };
 
-// compare both choices
-const div = document.createElement("div");
-
-let humanSelection = "";
-
-if (
-  (humanSelection === "rock" && computerSelection === "paper") ||
-  (humanSelection === "paper" && computerSelection === "scissors") ||
-  (humanSelection === "scissors" && computerSelection === "rock")
-) {
-  computerScore += 1;
-} else if (humanSelection === computerSelection) {
-  return tie;
-} humanScore += 1;
-
 // keep track of both choices and define round limit
 let humanScore = 0;
 let computerScore = 0;
+// compare both choices
+//const div = document.createElement("div");
+let comparison = function () {
+  if (
+    (humanSelection === "rock" && computerSelection === "paper") ||
+    (humanSelection === "paper" && computerSelection === "scissors") ||
+    (humanSelection === "scissors" && computerSelection === "rock")
+  ) {
+    computerScore += 1;
+  } else if (humanSelection === computerSelection) {
+    return "tie";
+  }
+  humanScore += 1;
+};
 
 // announce winner
+if (humanScore === 3) {
+  return "You win!";
+} else if (computerScore === 3) {
+  return "Computer wins!";
+}
